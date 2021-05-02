a56879c67cf9ca31cf42ce2f11c7a02b6142f510
shule517 <shule517@gmail.com>
2021-05-02 11:59:47 +0900

    Artworkモデルの追加

---
+++ app/models/artwork.rb
@@ -0,0 +1,10 @@
+# == Schema Information
+#
+# Table name: artworks
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Artwork < ApplicationRecord
+end
