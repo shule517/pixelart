01c12af8a4e745421a50ab29f82960df8f54482e
shule517 <shule517@gmail.com>
2021-05-03 19:30:31 +0900

    ハッシュタグ一覧ページを追加

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -1,4 +1,8 @@
 class HashtagsController < ApplicationController
+  def index
+    @hashtags = Hashtag.all.joins(:artworks).group(:hashtag_id).order("count(artwork_id) desc")
+  end
+
   def show
     @hashtag = Hashtag.find_by(name: params[:id])
     @artworks = @hashtag.artworks.sort_by { |artwork| artwork.favorite_count }.reverse.first(20)
