35e8797dc322213be4df0c1fa0a720daf2bdbd0e
shule517 <shule517@gmail.com>
2021-05-04 07:17:32 +0900

    画像以外はpaddingを追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @artworks = Artwork.order(favorite_count: :desc).first(20)
+    @artworks = Artwork.where(lang: 'ja').order(favorite_count: :desc).first(20)
   end
 end
