7ce5660789f37205483fc87050754153387d6b12
shule517 <shule517@gmail.com>
2021-05-05 07:12:19 +0900

    sidekiqを導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
   get ':lang', to: 'home#index', constraints: { lang: /ja|en/ }
   resources :hashtags, only: %i(index show)
+  mount Sidekiq::Web => '/sidekiq'
   get ':screen_name', to: 'artists#show', as: :artist
 end
