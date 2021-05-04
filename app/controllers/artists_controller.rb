6e8ea5958576bb183cffe9036377748f27dbebca
shule517 <shule517@gmail.com>
2021-05-04 16:55:28 +0900

    いいねしたアートのテンプレートを共通化

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,18 +2,19 @@ class ArtistsController < ApplicationController
   def show
     artworks = TwitterUserTweet.new(params[:screen_name]).user_artworks.sort_by {|artwork| artwork.favorite_count }.reverse.first(5)
     # TODO いいねをDB保存する
-    @like_artworks = TwitterUserTweet.new(params[:screen_name]).like_artworks.sort_by {|artwork| artwork.favorite_count }.reverse
+    like_artworks = TwitterUserTweet.new(params[:screen_name]).like_artworks.sort_by {|artwork| artwork.favorite_count }.reverse
 
     # TODO JOB化する
     artworks.each do |artwork|
       Artwork.create_from_tweet!(artwork)
     end
-    @like_artworks.each do |artwork|
+    like_artworks.each do |artwork|
       Artwork.create_from_tweet!(artwork)
     end
     # TODO JOB化する
 
     @artist = Artist.find_by!(screen_name: params[:screen_name])
     @artworks = @artist.artworks.order(favorite_count: :desc).limit(20).preload(:hashtags, :artist)
+    @like_artworks = Artwork.where(id: like_artworks.map(&:id)).order(favorite_count: :desc).preload(:hashtags, :artist)
   end
 end
