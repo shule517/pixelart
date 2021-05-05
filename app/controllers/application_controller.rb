e589d880c50f39df1adfa7697740891c6a936886
shule517 <shule517@gmail.com>
2021-05-05 11:23:27 +0900

    404ページを作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  if Rails.env.production?
+    rescue_from ActiveRecord::RecordNotFound,     with: :render_404
+  end
+
+  def render_404
+    render template: 'errors/404'
+  end
 end
