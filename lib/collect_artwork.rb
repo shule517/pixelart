edca369226f6c678801ff6ae2c019774d1bd18ef
shule517 <shule517@gmail.com>
2021-05-06 21:48:49 +0900

    リファクタリング

---
+++ lib/collect_artwork.rb
@@ -0,0 +1,51 @@
+class CollectArtwork
+  def initialize(screen_name)
+    @screen_name = screen_name
+    @client = TwitterClient.pixel.client
+  end
+
+  def exec
+    collect_artworks!
+  end
+
+  private
+
+  def collect_artworks!(max_id: nil)
+    tweets = nil
+    ActiveRecord::Base.transaction do
+      tweets = user_tweets(max_id: max_id)
+      puts "max_id: #{max_id} -> #{tweets.first&.id} - #{tweets.last&.id}"
+      return if tweets.blank?
+
+      select_artworks(tweets).each do |artwork|
+        artwork = Artwork.create_from_tweet!(artwork)
+        artwork.artist.update!(collect_tweet_oldest_id: tweets.last.id)
+      end
+      # statuses/user_timeline.jsonは、1秒間に1回まで
+      sleep(1)
+    end
+
+    collect_artworks!(max_id: tweets.last.id)
+  end
+
+  def select_artworks(tweets)
+    artworks = tweets
+      .select { |tweet| !tweet.retweeted_tweet? && !tweet.user_mentions? }
+      .select { |tweet| tweet.media.present? }
+      .select do |tweet|
+      tweet.hashtags.map(&:text).any? { |hashtag| %w(pixelart ドット絵 ドットピクト dotpict 10分ドット).include?(hashtag) }
+    end
+    artworks
+  end
+
+  def user_tweets(since_id: nil, max_id: nil)
+    params = { count: 200 }
+    params[:since_id] = since_id if since_id.present?
+    params[:max_id] = max_id if max_id.present?
+    tweets = @client.user_timeline(@screen_name, params)
+    if max_id.present?
+      tweets = tweets.drop(1) # max_idを指定した場合、先頭の1つは前回取得したものと同じためスキップする
+    end
+    tweets
+  end
+end
